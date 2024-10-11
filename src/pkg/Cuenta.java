a3f8f06259b74d84b4d27db740ca240c08e786a1
raulp <raulp@DESKTOP-9IMU10S>
2024-10-11 10:33:28 +0200

    2º commit CuentaTest: hacer que testIngresar funcione

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,26 @@
+package pkg;
+
+public class Cuenta {
+	
+	Double saldo;
+	
+	public Cuenta(Double i) {
+		
+	}
+
+	public void ingresar(Double i) {
+		
+		
+	}
+
+	public Double getSaldo() {
+		return 1d;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+
+}
